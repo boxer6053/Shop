2a121b11b3322efa50faf5557c6ab479ce84e5db
matrixsoft <macbook@MacBook-macbook.local>
2012-12-05 19:35:41 +0200

    share added

--- Shop/DetailProductViewController.m
+++ Shop/DetailProductViewController.m
@@ -1,9 +1,13 @@
 #import "DetailProductViewController.h"
 #import <Social/Social.h>
-
+#import <CoreFoundation/CoreFoundation.h>
 @interface DetailProductViewController ()
 {
     SLComposeViewController *slComposeSheet;
+    
+    UIButton *loginVK;
+    UIButton *postVK;
+    UIView *vkView;
 }
 @end
 
@@ -40,6 +44,7 @@
     [actionSheet setDelegate:self];
     [actionSheet addButtonWithTitle:@"Twitter"];
     [actionSheet addButtonWithTitle:@"Facebook"];
+    [actionSheet addButtonWithTitle:@"Vkontakte"];
     [actionSheet addButtonWithTitle:@"Cancel"];
     actionSheet.cancelButtonIndex = actionSheet.numberOfButtons - 1;
     
@@ -51,7 +56,6 @@
     if (buttonIndex == 0)
     {
         // Twitter
-        
         if ([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
         {
             slComposeSheet = [[SLComposeViewController alloc]init];
@@ -74,15 +78,7 @@
                      }
                          break;
                  }
-                 
              }];
-
-        }
-        else
-        {
-
-            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Twitter" message:@"You can't send a tweet right now, make sure your device has an internet connection and you have at least one Twitter account setup" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-            [alertView show];
         }
     }
     
@@ -110,19 +106,113 @@
                          [alertView show];
                      }
                          break;
-                 }
-
-                 
+                 }                 
              }];
         }
-        else
-        {
-        
-            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Facebook" message:@"You can't post on your Facebook's wall right now, make sure your device has an internet connection and you have at least one Facebook account setup" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
-        
-            [alertView show];
-        }
     }
-
+    
+    if (buttonIndex == 2)
+    {
+        //Vkontakte
+        _vkontakte = [Vkontakte sharedInstance];
+        _vkontakte.delegate = self;
+        
+        vkView = [[UIView alloc] initWithFrame:CGRectMake(0, 100, 320, 280)];
+        [vkView setBackgroundColor:[UIColor whiteColor]];
+        [self.view addSubview:vkView];
+        
+        loginVK = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [loginVK addTarget:self action:@selector(loginPressed) forControlEvents:UIControlEventTouchUpInside];
+        loginVK.frame = CGRectMake(vkView.frame.size.width/2 - 50, 120, 100, 40);
+        loginVK.hidden = NO;
+        [vkView addSubview:loginVK];
+        [self refreshButtonState];
+        
+        postVK = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        [postVK addTarget:self action:@selector(postImageWithTextAndLink) forControlEvents:UIControlEventTouchUpInside];
+        postVK.frame = CGRectMake (vkView.frame.size.width/2 - 50, 180, 100, 40);
+        postVK.hidden = YES;
+        [postVK setTitle:@"Post" forState:UIControlStateNormal];
+        [vkView addSubview:postVK];
+    }
 }
+
+- (void)refreshButtonState
+{
+    if (![_vkontakte isAuthorized])
+    {
+        [loginVK setTitle:@"Login"
+                 forState:UIControlStateNormal];
+        postVK.hidden = YES;
+    }
+    else
+    {
+        [loginVK setTitle:@"Logout"
+                 forState:UIControlStateNormal];
+        
+        postVK.hidden = NO;
+    }
+}
+
+-(void) loginPressed
+{
+    if (![_vkontakte isAuthorized])
+    {
+        [_vkontakte authenticate];
+    }
+    else
+    {
+        [_vkontakte logout];
+    }
+}
+
+- (void)postImageWithTextAndLink
+{
+    [_vkontakte postImageToWall:[UIImage imageNamed:@"test.jpg"]
+                           text:@"Vkontakte iOS SDK Trololo"
+                           link:[NSURL URLWithString:@"https://www.ex.ua"]];
+}
+
+#pragma mark - VkontakteDelegate
+
+- (void)vkontakteDidFailedWithError:(NSError *)error
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)showVkontakteAuthController:(UIViewController *)controller
+{
+    if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad)
+    {
+        controller.modalPresentationStyle = UIModalPresentationFormSheet;
+    }
+    
+    [self presentViewController:controller animated:YES completion:nil];
+}
+
+- (void)vkontakteAuthControllerDidCancelled
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)vkontakteDidFinishLogin:(Vkontakte *)vkontakte
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+    [self refreshButtonState];
+}
+
+- (void)vkontakteDidFinishLogOut:(Vkontakte *)vkontakte
+{
+    [self refreshButtonState];
+}
+
+
+- (void)vkontakteDidFinishPostingToWall:(NSDictionary *)responce
+{
+    NSLog(@"%@", responce);
+    UIAlertView *alertVK = [[UIAlertView alloc] initWithTitle:nil message:@"Posted successfully" delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [alertVK show];
+    [vkView removeFromSuperview];
+}
+
 @end
